906e9ae55da3b46759da843475b122915178b69e
Zt.wang <wangzitongnowb@163.com>
2018-05-26 09:33:57 +0800

    add flask example

--- flask/hello.py
+++ flask/hello.py
@@ -1,7 +1,8 @@
 # codeing=utf-8
-from flask import Flask
+from flask import Flask, jsonify, session, url_for, Response
 from flask import request
 from flask import render_template
+
 
 app = Flask(__name__)
 
@@ -13,8 +14,15 @@ def hello():
 
 @app.route('/showmap')
 def ShowMap(name=None):
-    return render_template('test1.html', name=name)
+    return render_template('track.html', name=name)
 
+@app.route('/ddd')
+def Showpass(name=None):
+	a = ['111','222',5]
+	b = 222
+	c = 333
+	#return jsonify(a, b, c)
+	return render_template('pass.html', name=a)
 
 def database(dbname):
     pass
