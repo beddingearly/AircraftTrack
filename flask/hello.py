4972c96db52af9d18cbc41a63e74418e7d982616
Zt.wang <wangzitongnowb@163.com>
2018-06-01 10:49:29 +0800

    add web function

--- flask/hello.py
+++ flask/hello.py
@@ -19,25 +19,31 @@ app = Flask(__name__)
 def hello():
     #a = SqliteOperator('../database/2018-4-15.db', 'ident', 'CCA102', '', '')
     #a.test()
-
     return render_template('front.html')
 
 
 @app.route('/showmap')
-def ShowMap(name=None):
-    return render_template('track.html', name=name)
+def showMap(name=None):
+    return render_template('bmap.html', name=name)
 
+
+
+@app.route('/showmap2')
+def showMap2():
+    return render_template('breal.html')
 
 @app.route('/ddd')
 def ShowHistory():
     keyvalue = str(request.args.get('ddd'))
     a = SqliteOperator('../database/2018-4-15.db', 'ident', keyvalue, '', '')
     aa = a.get_message()
-
     return jsonify(aa)
 
+@app.route('/message')
+def showMessage():
+    return render_template('message.html')
 
-# return render_template('pass.html', name=a)
+
 
 def database(dbname):
     pass
